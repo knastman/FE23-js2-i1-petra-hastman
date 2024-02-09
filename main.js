0d173fd2e15394547ef7e5a491924162baaebb7b
Petra Hastman <144622676+knastman@users.noreply.github.com>
2024-02-09 11:57:06 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,54 @@
 import { displayPlayerInfo } from "./modules/display.js";
 import { Fighter } from "./modules/fighter.js";
-import { FighterZangief, FighterType } from "./modules/fighterType.js";
+import { FighterZangief, fighterKunimitsu } from "./modules/fighterType.js";
 
 const form = document.querySelector('form');
 const attackButtons = document.querySelectorAll('.attackButton');
 
 // Deklarera här
 const fighterZangief = new FighterZangief();
-const newFighterType = new FighterType();
+const fighterkunimitsu = new fighterKunimitsu(); //ska det inte vara fighterKunimitsu där det står "figbtertype?""
+// const newFighterType = new FighterType();
 
+
+/*********************************
+ Get values from form
+**********************************/
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     
-    const playerOne = document.querySelector('#playerOne');
+    
+    const playerOne = document.querySelector('#playerOne'); 
     const playerTwo = document.querySelector('#playerTwo');
+    //Borde det inte vara value här istället
+
     const fighterTypeOne = document.querySelector('#fighterOne').value;
     const fighterTypeTwo = document.querySelector('#fighterTwo').value;
     const playerOneContainer = document.querySelector('.playerNameOne');
     const playerTwoContainer = document.querySelector('.playerNameTwo')
 
-    const typeAndPlayerOne = `${playerOne.value} as ${fighterTypeOne}`;
+    const typeAndPlayerOne = `${playerOne.value} as ${fighterTypeOne}`; 
     const typeAndPlayerTwo = `${playerTwo.value} as ${fighterTypeTwo}`;
+    //Eller att båda har value här? 
     
     playerOneContainer.innerHTML = '';
     playerTwoContainer.innerHTML = '';
 
     displayPlayerInfo(typeAndPlayerOne, playerOneContainer);
-    displayPlayerInfo(typeAndPlayerTwo, playerTwoContainer);
+    displayPlayerInfo(typeAndPlayerTwo, playerTwoContainer);  
+    //Ska man inte bygga hela typsnittet, utifrån val av fightertyp här, blir det inte konstigt att bara hämta en sak -> allt hänger ihop? 
     
-    
-    playerOne.value = '';
+    playerOne.value = ''; // Är detta för att tömma formuläret eller? 
     playerTwo.value = '';
+
+    // form.reset(); ska denna kanske vara med? 
 });
+
+
+/*********************************
+  Attack types handlers
+**********************************/
 
 // Lägg till ett klicklyssnare för varje knapp
 attackButtons.forEach(button => {
@@ -40,16 +56,16 @@ attackButtons.forEach(button => {
         // Hämta värden från data-attributen för den klickade knappen
         const attackName = button.getAttribute('data-attack-name');
         const attackDamage = button.getAttribute('data-attack-damage');
-        newFighterType.clearAttacks();
+        fighterkunimitsu.clearAttacks();
         fighterZangief.clearAttacks();
         fighterZangief.addAttack(attackName, parseInt(attackDamage));
-        newFighterType.addAttack(attackName, parseInt(attackDamage));
-        console.log(newFighterType.getAttackArray());
+        fighterkunimitsu.addAttack(attackName, parseInt(attackDamage));
+        console.log(fighterkunimitsu.getAttackArray());
        
     });
 });
 /* const newFighter = new FighterZangief();
-const newFighterTwo = new FighterType(); */
+const newFighterTwo = new fighterKunimitsu(); */
 /* newFighter.logInfo();
 newFighterTwo.logInfo();
 newFighter.addAttack('air manipulation', 40);
